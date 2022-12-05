ac1129a1773499c6a0d7aa3c8ddb20ad8f4d7250
Adnan <adnanmostafaprofessional@gmail.com>
2022-12-04 20:00:46 -0500

    Add Demo Page Object Model Class

--- CucumberJavaProject/src/test/java/StepDefinitions/LoginDemoSteps.java
+++ CucumberJavaProject/src/test/java/StepDefinitions/LoginDemoSteps.java
@@ -10,48 +10,42 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginDemoSteps {
 
-	WebDriver driver = null;
-
-	@Given("browser is open")
-	public void browser_is_open() {
-		// ChromeDriver setup along with timeouts and window.maximize
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
-		driver.manage().window().maximize();
-	}
-
-	@And("user is on login page")
-	public void user_is_on_login_page() {
-		// Write code here that turns the phrase above into concrete actions
-		driver.get("https://example.testproject.io/web/");
-	}
-
-	@When("^user enters (.*) and (.*)$")
-	public void user_enters_username_and_password(String username, String password) {
-		// Write code here that turns the phrase above into concrete actions
-		driver.findElement(By.xpath("//input[@id='name']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
-
-	}
-
-	@And("user clicks on login")
-	public void user_clicks_on_login() throws InterruptedException {
-		// Write code here that turns the phrase above into concrete actions
-		driver.findElement(By.xpath("//button[@id='login']")).click();
-		
-		Thread.sleep(2000);
-	}
-
-	@Then("user is navigated to home page")
-	public void user_is_navigated_to_home_page() throws InterruptedException {
-		driver.findElement(By.xpath("//button[@id='logout']")).isDisplayed();
-		
-		Thread.sleep(2000);
-
-		driver.close();
-		driver.quit();
-	}
+	/*
+	 * WebDriver driver = null;
+	 * 
+	 * @Given("browser is open") public void browser_is_open() { // ChromeDriver
+	 * setup along with timeouts and window.maximize
+	 * WebDriverManager.chromedriver().setup(); driver = new ChromeDriver();
+	 * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+	 * driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+	 * driver.manage().window().maximize(); }
+	 * 
+	 * @And("user is on login page") public void user_is_on_login_page() { // Write
+	 * code here that turns the phrase above into concrete actions
+	 * driver.get("https://example.testproject.io/web/"); }
+	 * 
+	 * @When("^user enters (.*) and (.*)$") public void
+	 * user_enters_username_and_password(String username, String password) { //
+	 * Write code here that turns the phrase above into concrete actions
+	 * driver.findElement(By.xpath("//input[@id='name']")).sendKeys(username);
+	 * driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+	 * 
+	 * }
+	 * 
+	 * @And("user clicks on login") public void user_clicks_on_login() throws
+	 * InterruptedException { // Write code here that turns the phrase above into
+	 * concrete actions
+	 * driver.findElement(By.xpath("//button[@id='login']")).click();
+	 * 
+	 * Thread.sleep(2000); }
+	 * 
+	 * @Then("user is navigated to home page") public void
+	 * user_is_navigated_to_home_page() throws InterruptedException {
+	 * driver.findElement(By.xpath("//button[@id='logout']")).isDisplayed();
+	 * 
+	 * Thread.sleep(2000);
+	 * 
+	 * driver.close(); driver.quit(); }
+	 */
 
 }
